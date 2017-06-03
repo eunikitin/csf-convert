25df751f479fe39b7ae431619f72bcf3395c3d4f
eunikitin <eu.nikitin@gmail.com>
2017-06-03 03:37:26 +0300

    Refactor aoa fixture

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -7,6 +7,9 @@ import sheet5x2Aoa from '../fixtures/5x2-missing/data-aoa';
 
 describe('sheetToAoa', () => {
   it('should convert data object to array of arrays', () => {
-    expect(library.sheetToAoa(sheet5x2Data)).to.deep.equal(sheet5x2Aoa);
+    expect(library.sheetToAoa(sheet5x2Data)).to.deep.equal([
+      sheet5x2Aoa[1],
+      sheet5x2Aoa[2],
+    ]);
   });
 });
